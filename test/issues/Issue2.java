b5aa8779c1ec353ac13163ef434aa48b62e0e889
jasonosgood <jason@jasonosgood.com>
2016-06-04 13:03:16 -0700

    copy edits

--- test/issues/Issue2.java
+++ test/issues/Issue2.java
@@ -12,17 +12,21 @@ https://github.com/jasonosgood/jsonacious/issues/2
  */
 public class Issue2
 {
-	public long data;
+	public byte a;
+	public int b;
+	public long c;
+	public float d;
+	public double e;
 
 	public static final void main( String[] args )
 		throws Exception
 	{
-		String json = "{ 'data' : 2 }".replace( '\'', '"' );
+		String json = "{ 'a' : 1, 'b' : 1, 'c' : 1, 'd' : 1, 'e' : 1  }".replace( '\'', '"' );
 
 		JSONReader reader = new JSONReader();
 
 		Issue2 issue2 = reader.parse( json, Issue2.class );
-		System.out.println( issue2.data );
+		System.out.println( issue2.a );
 
 	}
 }
